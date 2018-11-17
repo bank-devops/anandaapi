44f2d381afa1ab3b127befd4df9590e25b8b84e1
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-17 17:17:29 +0700

    Update activity.php

--- activity.php
+++ activity.php
@@ -1,6 +1,6 @@
 <?php
-$activity = array("id"=>"1",
-                  "name"=>"activity1",
+$activity = array("campaign_id"=>"1",
+                  "display_name"=>"activity1",
                   "startdate"=>"2018-11-1 00:00:00",
                   "finishdate"=>"2018-11-30 23:59:59",
                   "banner"=>"https://en.wikipedia.org/wiki/IMAGE#/media/File:IMAGE_(Explorer_78).jpg"
