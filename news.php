7fad4f5f3c4ddf0a7e205ded3fe6648459e6101b
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-17 17:24:46 +0700

    Create news.php

---
+++ news.php
@@ -0,0 +1,16 @@
+<?php
+$news = array("news_id"=>"1",
+                  "display_name"=>"Image Topic",
+                  "Topic"=>"Promote / Event"ม
+                  "detail_short"=>"IMAGE was the first spacecraft dedicated to imaging the Earth's magnetosphere.",
+                  "detail_full"=>"IMAGE was the first spacecraft dedicated to imaging the Earth's magnetosphere.[1] IMAGE was a spacecraft developed by the Medium-Class Explorers (MIDEX) program, and it was the first spacecraft dedicated to observing the magnetosphere of the Earth, producing comprehensive global images of plasma in the inner magnetosphere. The IMAGE craft was placed in a 1,000×46,000 km orbit around the Earth, with an inclination of 90° (passing over the poles) and a 14.2 hour period.By acquiring images every 2 minutes in wavelengths invisible to the human eye, it allowed detailed study of the interaction of the solar wind with the magnetosphere and the magnetosphere's response during a magnetic storm. From its distant orbit, the spacecraft produced a wealth of images of the previously invisible region of space in the inner magnetosphere, exceeded all its scientific goals. A senior review in 2005, just previous to its loss, described the mission as "extremely productive",[14] having confirmed several theoretical predictions (e.g., plasmasphere plumes, pre-midnight ring-current injection, and continuous antiparallel reconnection), discovered numerous new and unanticipated phenomena (e.g., plasmasphere shoulders, subauroral proton arcs, and a secondary interstellar neutral atom stream), and answered a set of outstanding questions regarding the source region of kilometric continuum radiation, the role of solar wind pressure pulses in ionospheric outflow, and the relationship between proton and electron auroras during substorms.[14][15] When the spacecraft went silent in December 2005, it had already been approved a mission extension until 2010.[14]Costs for IMAGE are estimated at US$132 million, including the spacecraft, instruments, launch vehicle, and ground operations.[1]",
+                  "linkout"=>"https://en.wikipedia.org/wiki/IMAGE",
+                  "Period_display"=>"10:00 - 22:00",
+                  "date_display"=>"วันจันทร์ที่ 15 พฤศิกายน 2561",
+                  "place"=>"สาธร",
+                  "startdate"=>"2018-11-1 00:00:00",
+                  "enddate"=>"2018-11-30 23:59:59",
+                  "banner"=>"https://en.wikipedia.org/wiki/IMAGE#/media/File:IMAGE_(Explorer_78).jpg"
+                 );
+echo json_encode(array($news,$news,$news,$news,$news));
+?>
