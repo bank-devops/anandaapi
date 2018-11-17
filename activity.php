739a7eb56951bfff674cb40b4f2ba2ffbb376e8e
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-17 20:36:34 +0700

    Update activity.php

--- activity.php
+++ activity.php
@@ -3,7 +3,12 @@ $activity = array("campaign_id"=>"1",
                   "display_name"=>"activity1",
                   "startdate"=>"2018-11-1 00:00:00",
                   "finishdate"=>"2018-11-30 23:59:59",
-                  "banner"=>"https://en.wikipedia.org/wiki/IMAGE#/media/File:IMAGE_(Explorer_78).jpg"
+                  "banner"=>"https://www.w3schools.com/html/img_chania.jpg"
                  );
-echo json_encode(array($activity,$activity,$activity,$activity,$activity));
+$respone = array(
+                  "status"=>200,
+                  "message"=>"Success",
+                  "data"=> array($activity,$activity,$activity,$activity,$activity)
+                );
+echo json_encode($respone);
 ?>
