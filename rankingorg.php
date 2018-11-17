28a761edf3cd04c44252b3c9ce3b0c28ba9566f6
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-17 21:21:45 +0700

    Update rankingorg.php

--- rankingorg.php
+++ rankingorg.php
@@ -1,11 +1,24 @@
 <?
 $company = array("company_id"=>"1",
                   "company_name"=>"company1",
-                  "company_icon"=>"https://en.wikipedia.org/wiki/IMAGE#/media/File:Aurora_as_seen_by_IMAGE.PNG",
+                  "company_icon"=>"https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png",
                   "company_stepsummary"=>"100000",
                   "company_person"=>"200",
                   "company_rank"=>"1."
                  );
-echo json_encode(array($company,$company,$company,$company,$company));
+$event = array("campaign_id"=>"1",
+               "display_name"=>"activity1",
+               "startdate"=>"2018-11-1 00:00:00",
+               "finishdate"=>"2018-11-30 23:59:59",
+               "banner"=>"https://www.w3schools.com/html/img_chania.jpg",
+               "ranking"=>array($company ,$company ,$company ,$company )
+              );
+$respone = array(
+                  "status"=>200,
+                  "message"=>"Success",
+                  "data"=> array($event ,$event ,$event  );
+                );
+echo json_encode($respone);
+
 
 ?>
