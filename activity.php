b94d35bd29957f0b4310355b83d9f61538d82efd
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-17 17:16:21 +0700

    Update activity.php

--- activity.php
+++ activity.php
@@ -5,5 +5,5 @@ $activity = array("id"=>"1",
                   "finishdate"=>"2018-11-30 23:59:59",
                   "banner"=>"https://en.wikipedia.org/wiki/IMAGE#/media/File:IMAGE_(Explorer_78).jpg"
                  );
-echo json_encode($activity,$activity,$activity,$activity,$activity);
+echo json_encode(array($activity,$activity,$activity,$activity,$activity));
 ?>
