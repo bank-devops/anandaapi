dfb56b7b7226f6dc76e23be5d05180b7fc365025
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-18 10:32:56 +0700

    Update activity.php

--- activity.php
+++ activity.php
@@ -3,7 +3,7 @@ $activity = array("campaign_id"=>"1",
                   "display_name"=>"activity1",
                   "startdate"=>"2018-11-1 00:00:00",
                   "finishdate"=>"2018-11-30 23:59:59",
-                  "banner"=>"https://www.w3schools.com/html/banner-ananda.png"
+                  "banner"=>"https://golangdevbank.herokuapp.com//banner-ananda.png"
                  );
 $respone = array(
                   "status"=>200,
